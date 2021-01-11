3b7c29863218545d78900bbba1be68b82ed3e50d
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-11 04:06:23 -0500

    formatted time left

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,15 +1,19 @@
 import React, {useState} from "react"
+import moment from "moment"
+import momentDurationFormatSetup from "moment-duration-format"
+
+momentDurationFormatSetup(moment);
 
 export default function Timer(props) {
 
     let {sessionLengthInSeconds} = props; 
     let [timeLeft, setTimeLeft] = useState(sessionLengthInSeconds);
-    
+    let formattedTimeLeft = moment.duration(timeLeft, "s").format("mm:ss");
+
     return (
         <div>
             <p>Time Left</p>
-            <div>{timeLeft}</div>
+            <div>{formattedTimeLeft}</div>
         </div>
-        
     )
 }
