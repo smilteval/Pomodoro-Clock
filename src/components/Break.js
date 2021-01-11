be97e92f3e63ec6333ee5b7c288cf5b3a56daaa6
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-11 03:23:06 -0500

    added break increment/decrement buttons

--- src/components/Break.js
+++ src/components/Break.js
@@ -7,11 +7,31 @@ export default function Break() {
     let [breakLengthInSeconds, setBreakLength] = useState(300);    
     let breakLengthInMinutes = moment.duration(breakLengthInSeconds,"s").minutes();
     
+    //increases the break timer by 1 minute
+
+    let incrementBreakLength = () =>{
+        setBreakLength(breakLengthInSeconds + 60);
+    }
+
+    //decreases the break timer by 1 minute
+    let decrementBreakLength = () =>{
+        let newBreakLength = breakLengthInSeconds - 60;
+
+        if(newBreakLength < 0){
+            setBreakLength(0);
+        }
+        else{
+            setBreakLength(newBreakLength);
+        }
+    }
+
     return (
         <div>
             <p>Break</p>
             <p>{breakLengthInSeconds}</p>
             <p>{breakLengthInMinutes}</p>
+            <button id="increment-break" onClick={incrementBreakLength}>+</button>
+            <button id="decrement-break" onClick={decrementBreakLength}>-</button>
         </div>
     )
 }
