5b1f805a5c6a219d9792c9961819ea64a4155aec
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-11 05:41:35 -0500

    refactored the code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import Break from "./components/Break"
 import Session from "./components/Session"
 import Timer from "./components/Timer"
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 function App() {
 
   let [sessionLengthInSeconds, setSessionLength] = useState(1500);    
   let [breakLengthInSeconds, setBreakLength] = useState(300); 
+  let [timerStatus, setTimerStatus] = useState(null);
+  let [currentSessionType, setCurrentSessionType] = useState("Session");
+  let [timeLeft, setTimeLeft] = useState(sessionLengthInSeconds);
 
   //increases the session timer by 1 minute
   let incrementSessionLength = () =>{
@@ -43,6 +46,62 @@ function App() {
       }
   }
 
+  //resets the timer
+
+  let handleResetButton = () => {
+
+    //clear the interval
+
+  }
+
+  //change time whenever session length changes
+  useEffect(() => {
+    setTimeLeft(sessionLengthInSeconds);
+  }, [sessionLengthInSeconds])
+
+  let handleStartStopClick = () => {
+
+    if(timerStatus !== null){ //if the timer has started
+        
+        //Stop the timer
+        clearInterval(timerStatus);
+
+        //indicate that the timer stopped
+        setTimerStatus(null);
+    }
+    else{ //if the timer hasn't started
+
+        //start the timer 
+        let newTimerStatus = setInterval(() => {
+            setTimeLeft(prevTimeLeft => {
+
+                //decrement time left by one every second
+                let newTimeLeft = prevTimeLeft - 1;
+
+                if(newTimeLeft >= 0){
+                    return newTimeLeft;
+                }
+
+                //once the time runs out
+
+                //if in session, switch to break
+                if(currentSessionType === "Session"){
+                    setCurrentSessionType("Break");
+                    setTimeLeft(breakLengthInSeconds);
+                }
+
+                //if on break, switch to session
+                if(currentSessionType === "Break"){
+                    setCurrentSessionType("Session");
+                    setTimeLeft(sessionLengthInSeconds);
+                }
+            })
+        }, 100); //in milliseconds
+
+        //indicate that the timer started
+        setTimerStatus(newTimerStatus);
+    }
+}
 
   return (
     <div className="App">
@@ -60,6 +119,10 @@ function App() {
       <Timer
         sessionLengthInSeconds = {sessionLengthInSeconds}
         breakLengthInSeconds = {breakLengthInSeconds}
+        currentSessionType = {currentSessionType}
+        handleStartStopClick = {handleStartStopClick}
+        timerStatus = {timerStatus}
+        timeLeft = {timeLeft}
       />
     </div>
   );
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,3 @@
-import React, {useState, useEffect} from "react"
 import moment from "moment"
 import momentDurationFormatSetup from "moment-duration-format"
 
@@ -7,66 +6,15 @@ momentDurationFormatSetup(moment);
 export default function Timer(props) {
 
     let {
-        sessionLengthInSeconds,
-        breakLengthInSeconds,
+        currentSessionType,
+        handleStartStopClick,
+        timerStatus,
+        timeLeft,
     } = props;
-
-    let [timerStatus, setTimerStatus] = useState(null);
-    let [timeLeft, setTimeLeft] = useState(sessionLengthInSeconds);
-    let [currentSessionType, setCurrentSessionType] = useState("Session");
 
     //turn time into mm::dd format
     let formattedTimeLeft = moment.duration(timeLeft, "s").format("mm:ss", {trim: false});
-
-    //change time whenever session length changes
-    useEffect(() => {
-        setTimeLeft(sessionLengthInSeconds);
-    }, [sessionLengthInSeconds])
-
-    let handleStartStopClick = () => {
-
-        if(timerStatus !== null){ //if the timer has started
-            
-            //Stop the timer
-            clearInterval(timerStatus);
-
-            //indicate that the timer stopped
-            setTimerStatus(null);
-        }
-        else{ //if the timer hasn't started
-
-            //start the timer 
-            let newTimerStatus = setInterval(() => {
-                setTimeLeft(prevTimeLeft => {
-
-                    //decrement time left by one every second
-                    let newTimeLeft = prevTimeLeft - 1;
-
-                    if(newTimeLeft >= 0){
-                        return newTimeLeft;
-                    }
-
-                    //once the time runs out
-
-                    //if in session, switch to break
-                    if(currentSessionType === "Session"){
-                        setCurrentSessionType("Break");
-                        setTimeLeft(breakLengthInSeconds);
-                    }
-
-                    //if on break, switch to session
-                    if(currentSessionType === "Break"){
-                        setCurrentSessionType("Session");
-                        setTimeLeft(sessionLengthInSeconds);
-                    }
-                })
-            }, 100); //in milliseconds
-
-            //indicate that the timer started
-            setTimerStatus(newTimerStatus);
-        }
-    }
-
+    
     return (
         <div>
             <p>Time Left</p>
