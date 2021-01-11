8f402d35329090275dea96db5708defe51ec7e76
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-11 04:25:23 -0500

    decrements timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -8,19 +8,38 @@ export default function Timer(props) {
 
     let {sessionLengthInSeconds} = props; 
     let [timeLeft, setTimeLeft] = useState(sessionLengthInSeconds);
-    
+
+    //turn time into mm::dd format
+    let formattedTimeLeft = moment.duration(timeLeft, "s").format("mm:ss");
+
     //change time whenever session length changes
     useEffect(() => {
         setTimeLeft(sessionLengthInSeconds);
     }, [sessionLengthInSeconds])
 
-    //turn time into mm::dd format
-    let formattedTimeLeft = moment.duration(timeLeft, "s").format("mm:ss");
+    let handleStartStopClick = () => {
+        
+        //decrement time left by one every second
+        setInterval(() => {
+            setTimeLeft(prevTimeLeft => {
+
+                let newTimeLeft = prevTimeLeft - 1;
+
+                if(newTimeLeft >= 0){
+                    return newTimeLeft;
+                }
+                else{
+                    return prevTimeLeft;
+                }
+            })
+        }, 1000); //in milliseconds
+    }
 
     return (
         <div>
             <p>Time Left</p>
             <div>{formattedTimeLeft}</div>
+            <button id = "start" onClick={handleStartStopClick}>Start</button>
         </div>
     )
 }
